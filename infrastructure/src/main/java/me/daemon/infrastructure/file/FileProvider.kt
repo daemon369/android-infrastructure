483a64052dab99b7d03f667ee63b6313d7b395b0
daemon369 <daemon336699@gmail.com>
2021-06-15 15:26:21 +0800

    add Context.authority property

--- infrastructure/src/main/java/me/daemon/infrastructure/file/FileProvider.kt
+++ infrastructure/src/main/java/me/daemon/infrastructure/file/FileProvider.kt
@@ -5,7 +5,13 @@ import android.net.Uri
 import androidx.core.content.FileProvider
 import java.io.File
 
+@Suppress("SpellCheckingInspection")
+private const val authoritySuffix = ".daemon.fileprovider"
+
 class FileProvider : FileProvider()
 
+val Context.authority
+    get() = "$packageName$authoritySuffix"
+
 fun Context.fileToUri(file: File): Uri =
-        FileProvider.getUriForFile(this, "$packageName.daemon.fileprovider", file)
+        FileProvider.getUriForFile(this, authority, file)
