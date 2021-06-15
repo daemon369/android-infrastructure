55e241bf7d11bb74719700fbdcae01b8be1a1542
daemon369 <daemon336699@gmail.com>
2021-06-15 15:11:12 +0800

    refactor build scripts

--- buildSrc/src/main/kotlin/me/daemon/plugin/Libraries.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Libraries.kt
@@ -2,31 +2,39 @@ package me.daemon.plugin
 
 object Libraries {
 
-    private const val kotlinVersion = "1.4.32"
-    private const val coreVersion = "1.5.0"
-    private const val appCompatVersion = "1.3.0"
-    private const val gradlePluginVersion = "4.1.2"
+    @Suppress("ClassName")
+    private object v {
+        const val kotlin = "1.4.32"
+        const val core = "1.5.0"
+        const val appCompat = "1.3.0"
+        const val constraintLayout = "2.0.4"
+        const val gradlePlugin = "4.1.2"
+        const val junit = "4.13.2"
+        const val xJunit = "1.1.2"
+        const val espresso = "3.3.0"
+    }
 
     object A {
-        const val gradle = "com.android.tools.build:gradle:$gradlePluginVersion"
+        const val gradle = "com.android.tools.build:gradle:${v.gradlePlugin}"
     }
 
     object X {
-        const val core = "androidx.core:core:$coreVersion"
-        const val coreKtx = "androidx.core:core-ktx:$coreVersion"
-        const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
+        const val core = "androidx.core:core:${v.core}"
+        const val coreKtx = "androidx.core:core-ktx:${v.core}"
+        const val appCompat = "androidx.appcompat:appcompat:${v.appCompat}"
+        const val constraint = "androidx.constraintlayout:constraintlayout:${v.constraintLayout}"
     }
 
     object K {
-        const val std = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-        const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        const val std = "org.jetbrains.kotlin:kotlin-stdlib:${v.kotlin}"
+        const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${v.kotlin}"
         const val coroutines = ""
     }
 
     object T {
-        const val junit = "junit:junit:4.13.2"
-        const val xJunit = "androidx.test.ext:junit:1.1.2"
-        const val espresso = "androidx.test.espresso:espresso-core:3.3.0"
+        const val junit = "junit:junit:${v.junit}"
+        const val xJunit = "androidx.test.ext:junit:${v.xJunit}"
+        const val espresso = "androidx.test.espresso:espresso-core:${v.espresso}"
     }
 
 }
