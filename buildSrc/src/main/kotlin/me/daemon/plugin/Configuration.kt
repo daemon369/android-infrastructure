6f754b88ea1c7564162f7313c433a5f9bf6ae5b3
daemon369 <daemon336699@gmail.com>
2021-06-16 14:22:38 +0800

    prepare for publishing

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -3,7 +3,7 @@ package me.daemon.plugin
 object Configuration {
     const val minSdkVersion = 19
     const val compileSdkVersion = 30
-    const val targetSdkVersion = 28
+    const val targetSdkVersion = 29
     const val buildToolsVersion = "30.0.3"
     const val groupId = "io.github.daemon369"
     const val artifactId = "android-infrastructure"
--- buildSrc/src/main/kotlin/me/daemon/plugin/Libraries.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Libraries.kt
@@ -9,9 +9,15 @@ object Libraries {
     private object v {
         const val kotlin = "1.4.32"
         const val coroutines = "1.4.2"
+
         const val core = "1.5.0"
         const val appCompat = "1.3.0"
+        const val lifecycle = "2.3.1"
+        const val activity = "1.2.3"
+        const val fragment = "1.3.4"
+        const val navigation = "2.3.5"
         const val constraintLayout = "2.0.4"
+
         const val gradlePlugin = "4.1.2"
         const val junit = "4.13.2"
         const val xJunit = "1.1.2"
@@ -32,6 +38,10 @@ object Libraries {
         const val core = "androidx.core:core:${v.core}"
         const val coreKtx = "androidx.core:core-ktx:${v.core}"
         const val appCompat = "androidx.appcompat:appcompat:${v.appCompat}"
+        const val lifecycleJava8 = "androidx.lifecycle:lifecycle-common-java8:${v.lifecycle}"
+        const val activity = "androidx.activity:activity:${v.activity}"
+        const val fragment = "androidx.fragment:fragment:${v.fragment}"
+        const val fragmentKtx = "androidx.fragment:androidx.fragment:fragment-ktx:${v.fragment}"
         const val constraint = "androidx.constraintlayout:constraintlayout:${v.constraintLayout}"
     }
 
