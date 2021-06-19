ed4bb99c9b95916181458f3996c19feb9d9817ca
daemon369 <daemon336699@gmail.com>
2021-06-19 15:59:20 +0800

    refactor build script

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -8,4 +8,18 @@ object Configuration {
     const val groupId = "io.github.daemon369"
     const val artifactId = "android-infrastructure"
     const val version = "0.1.1"
+
+    @Suppress("ClassName")
+    object pom {
+        const val name = artifactId
+        const val description = "Android infrastructure"
+        const val url = "https://github.com/daemon369/android-infrastructure"
+
+        @Suppress("ClassName")
+        object scm {
+            const val connection = "scm:git:github.com/daemon369/android-infrastructure.git"
+            const val developerConnection = "scm:git:ssh://github.com/daemon369/android-infrastructure.git"
+            const val url = "https://github.com/daemon369/android-infrastructure/tree/main"
+        }
+    }
 }
