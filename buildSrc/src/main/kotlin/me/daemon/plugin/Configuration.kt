ce7688dff014bf6c364cf2a55934261bb52d611b
daemon369 <daemon336699@gmail.com>
2021-06-21 10:05:44 +0800

    refactor build script for multi-library

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -6,20 +6,4 @@ object Configuration {
     const val targetSdkVersion = 29
     const val buildToolsVersion = "30.0.3"
     const val groupId = "io.github.daemon369"
-    const val artifactId = "android-infrastructure"
-    const val version = "0.1.2"
-
-    @Suppress("ClassName")
-    object pom {
-        const val name = artifactId
-        const val description = "Android infrastructure"
-        const val url = "https://github.com/daemon369/android-infrastructure"
-
-        @Suppress("ClassName")
-        object scm {
-            const val connection = "scm:git:github.com/daemon369/android-infrastructure.git"
-            const val developerConnection = "scm:git:ssh://github.com/daemon369/android-infrastructure.git"
-            const val url = "https://github.com/daemon369/android-infrastructure/tree/main"
-        }
-    }
 }
