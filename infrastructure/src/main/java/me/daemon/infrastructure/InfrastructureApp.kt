a3c3628f42c864f8f4216a1ccf3a08228a6c442b
daemon369 <daemon336699@gmail.com>
2021-09-04 19:33:45 +0800

    deprecation

--- infrastructure/src/main/java/me/daemon/infrastructure/InfrastructureApp.kt
+++ infrastructure/src/main/java/me/daemon/infrastructure/InfrastructureApp.kt
@@ -28,47 +28,91 @@ open class InfrastructureApp : me.daemon.infrastructure.application.Infrastructu
 val application: InfrastructureApp
     get() = me.daemon.infrastructure.application.application as InfrastructureApp
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.debuggable instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.debuggable")
+)
 val Context.debuggable: Boolean
     get() = me.daemon.infrastructure.application.debuggable
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.packageInfo instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.packageInfo")
+)
 val Context.packageInfo: PackageInfo
     get() = me.daemon.infrastructure.application.packageInfo
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.versionCode instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.versionCode")
+)
 @Suppress("DEPRECATION")
 val Context.versionCode: Int
     get() = me.daemon.infrastructure.application.versionCode
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.longVersionCode instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.longVersionCode")
+)
 val Context.longVersionCode: Long
     @RequiresApi(Build.VERSION_CODES.P)
     get() = me.daemon.infrastructure.application.longVersionCode
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.versionName instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.versionName")
+)
 val Context.versionName: String
     get() = me.daemon.infrastructure.application.versionName
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.debuggable instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.debuggable")
+)
 @RequireInfrastructureApp
 val debuggable: Boolean
     get() = me.daemon.infrastructure.application.debuggable
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.packageInfo instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.packageInfo")
+)
 @RequireInfrastructureApp
 val packageInfo: PackageInfo
     get() = me.daemon.infrastructure.application.packageInfo
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.versionCode instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.versionCode")
+)
 @RequireInfrastructureApp
 @Suppress("unused")
 val versionCode: Int
     get() = me.daemon.infrastructure.application.versionCode
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.longVersionCode instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.longVersionCode")
+)
 @Suppress("unused")
 @RequireInfrastructureApp
 val longVersionCode: Long
     @RequiresApi(Build.VERSION_CODES.P)
     get() = me.daemon.infrastructure.application.longVersionCode
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.versionName instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.versionName")
+)
 @Suppress("unused")
 @RequireInfrastructureApp
 val versionName: String
     get() = me.daemon.infrastructure.application.versionName
 
+@Deprecated(
+    message = "use me.daemon.infrastructure.application.packageName instead",
+    replaceWith = ReplaceWith("me.daemon.infrastructure.application.packageName")
+)
 @RequireInfrastructureApp
 @Suppress("unused")
 val packageName: String
