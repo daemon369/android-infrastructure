e8d93027d36a5cda92cb47c91272a1e5255e99b2
daemon369 <daemon336699@gmail.com>
2022-05-04 11:55:25 +0800

    refactor build script

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,31 +1,51 @@
+import me.daemon.plugin.Configuration
+import me.daemon.gradle.PublishInfo
+import me.daemon.gradle.PublishInfo.Pom
+import me.daemon.gradle.PublishInfo.Pom.Scm
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
+    id("maven-publish")
+    id("signing")
 }
 
-ext {
-    set("artifactId", "android-infrastructure")
-    set("artifactVersion", "0.3.0")
+val artifactGroupId: String by project
 
-    set(
-        "pom",
-        mapOf(
-            "name" to get("artifactId"),
-            "description" to "Android infrastructure",
-            "url" to "https://github.com/daemon369/android-infrastructure",
-            "scm" to mapOf(
-                "connection" to "scm:git:git://github.com/daemon369/android-infrastructure.git",
-                "developerConnection" to "scm:git:ssh://github.com/daemon369/android-infrastructure.git",
-                "url" to "https://github.com/daemon369/android-infrastructure/tree/main",
-            )
+val publishInfo = PublishInfo(
+    artifactId = "android-infrastructure",
+    artifactVersion = "0.3.0",
+    pom = Pom(
+        name = "android-infrastructure",
+        description = "Android infrastructure",
+        url = "https://github.com/daemon369/android-infrastructure",
+        scm = Scm(
+            connection = "scm:git:git://github.com/daemon369/android-infrastructure.git",
+            developerConnection = "scm:git:ssh://github.com/daemon369/android-infrastructure.git",
+            url = "https://github.com/daemon369/android-infrastructure/tree/main",
         )
     )
-}
+)
 
-apply(from = "$rootDir/gradle/library_base.gradle")
-apply(from = "$rootDir/gradle/maven-publish.gradle")
+apply(from = "$rootDir/gradle/base.gradle")
 
 android {
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        kotlinOptions.freeCompilerArgs =
+            listOf("-module-name", "${artifactGroupId}.${publishInfo.artifactId}")
+    }
     buildFeatures {
         buildConfig = false
     }
@@ -36,4 +56,62 @@ dependencies {
     implementation("io.github.daemon369:annotation:0.1.0")
     implementation("io.github.daemon369:android-infrastructure-application:1.2.0")
 //    implementation project(":annotation")
+}
+
+afterEvaluate {
+    publishing {
+
+        publications {
+            repositories {
+                maven {
+                    url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+                    credentials {
+                        username = project.extra["ossrhUsername"]!!.toString()
+                        password = project.extra["ossrhPassword"]!!.toString()
+                    }
+                }
+            }
+
+            create<MavenPublication>("release") {
+                groupId = Configuration.groupId
+                artifactId = publishInfo.artifactId
+                version = publishInfo.artifactVersion
+                if (plugins.findPlugin("com.android.library") != null) {
+                    from(components["release"])
+                } else {
+                    from(components["java"])
+                }
+
+                pom {
+                    name.set(publishInfo.pom.name)
+                    description.set(publishInfo.pom.description)
+                    url.set(publishInfo.pom.url)
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("daemon")
+                            name.set("Daemon")
+                            email.set("daemon336699@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set(publishInfo.pom.scm.connection)
+                        developerConnection.set(publishInfo.pom.scm.developerConnection)
+                        url.set(publishInfo.pom.scm.url)
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+signing {
+    sign(publishing.publications)
 }
